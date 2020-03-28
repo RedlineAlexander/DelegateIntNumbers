85d75946eda6e5e7a83aca2b250633b6279d2260
Олександр Кухар <redlinerailmetro@gmail.com>
2020-03-28 17:51:08 +0200

    Добавьте файлы проекта.

---
+++ DelegateAndLambdaAnonymousFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegateAndLambdaAnonymousFunction
+{
+    public delegate string StopFake(string fake);
+    public delegate string FakeNewsEvent(object sender, EventArgs e);
+
+    class Program
+    {
+        public event Action OnBase = delegate {};
+        public void RaiseFM()
+        {
+            OnBase();
+        }
+        static void Main(string[] args)
+        {
+
+
+            Program program = new Program();
+            StopFake stopFake = (fake) => fake;
+            var result = stopFake("FAKE JAVA NEWS");
+            Console.WriteLine(result);
+            program.OnBase += () => Console.WriteLine("FAKE SUB 1");
+            program.OnBase += () => Console.WriteLine("FAKE SUB 2");
+            program.RaiseFM();
+             
+
+        Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DelegateIntNumbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DelegateIntNumbers
+
+{
+    public delegate String MyIntegerDelegate(int Myinteger);
+
+    class Program
+    {
+        public string DetermineStringSymbolMathMethod(int MyInteger)
+        {
+            if (MyInteger > 0)
+            {
+                return ("+");
+            }
+            if(MyInteger < 0)
+            {
+                return ("-");
+            }
+            return (" ");
+        }
+
+        public static string DetermineStringMethod(int MyInteger)
+        {
+            if (MyInteger > 0)
+            {
+                return ("Positive");
+            }
+            else
+            {
+                return ("Negative");
+            }
+            return ("");
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            MyIntegerDelegate MYI = new MyIntegerDelegate(program.DetermineStringSymbolMathMethod);
+            MyIntegerDelegate MYI32 = new MyIntegerDelegate(Program.DetermineStringMethod);
+
+
+            Console.WriteLine(MYI(2), MYI32(2));
+            Console.WriteLine(MYI(2), MYI32(2));
+            Console.WriteLine(MYI(2), MYI32(2));
+           // Console.WriteLine();
+          //  Console.WriteLine();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
