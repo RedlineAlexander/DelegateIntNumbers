380328d7ee06d37ed65cdb40216fbdc8a3d2839c
Олександр Кухар <redlinerailmetro@gmail.com>
2020-04-02 21:00:51 +0300

    ITEA C# New Topics

---
+++ DelegateRichterAnalyze/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO
+using System.Windows.Forms;
+namespace DelegateRichterAnalyze
+{
+    internal delegate void FeedBack(Int32 value);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticDelegateDemo();
+            InstanceDelegateDemo();
+            ChainDelegateMethodDemoOne(new Program());
+            ChainDelegateMethodDemoTwo(new Program());
+            Console.WriteLine("Hello World!");
+
+        }
+        private static void StaticDelegateDemo()
+        {
+
+        }
+        private static void InstanceDelegateDemo()
+        {
+
+        }
+        private static void ChainDelegateMethodDemoOne(Program p)
+        {
+            FeedBack feedBack = new FeedBack(FeedBackToConsole);
+            FeedBack feedBack1 = new FeedBack(p.FeedBackToConsole);
+            FeedBack feedBack2 = new FeedBack(FromMessageToBox);
+
+            FeedBack feedBackChain = null;
+
+
+            feedBackChain += feedBack;
+            feedBackChain += feedBack1;
+            feedBackChain += feedBack2;
+            Counter(1, 2, feedBackChain);
+
+            Console.WriteLine();
+            feedBackChain -= new FeedBack(FromMessageToBox);
+            Counter(1, 2, feedBackChain);
+
+        }
+        private static void ChainDelegateMethodDemoTwo(Program p)
+        {
+            FeedBack feedBack = new FeedBack(FeedBackToConsole);
+            FeedBack feedBack1 = new FeedBack(p.FeedBackToFileConsole);
+            FeedBack feedBack2 = new FeedBack(FromMessageToBox);
+
+            FeedBack feedBackChain = null;
+
+            feedBackChain = (FeedBack)Delegate.Combine(feedBackChain, feedBack);
+            feedBackChain = (FeedBack)Delegate.Combine(feedBackChain, feedBack1);
+            feedBackChain = (FeedBack)Delegate.Combine(feedBackChain, feedBack2);
+
+            Console.WriteLine();
+            feedBackChain = (FeedBack)Delegate.Remove(feedBackChain, new FeedBack(feedBackChain));
+            Counter(1, 2, feedBackChain);
+
+
+
+
+            
+        }
+        private static void FeedBackToConsole(Int32 value)
+        {
+            Console.WriteLine(" " + value);
+        }
+
+        private static void Counter(Int32 from, Int32 to, FeedBack feedBack)
+        {
+            for (int i = from; i <= to; i++)
+            {
+                if(feedBack != null)
+                {
+                    feedBack(i);
+                }
+            }
+        }
+        private static void FromMessageToBox(Int32 value)
+
+        { 
+            
+        }
+        private void FeedBackToFileConsole(Int32 value)
+        {
+            using (StreamWriter sw = new StreamWriter("Status", true))
+            {
+                sw.WriteLine("ItemFile" +value);
+            }
+        }
+    }
+}
---
+++ LINQLINQLINQLINQLINQ/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LINQLINQLINQLINQLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var AnonymoMamaMioPandemio = new {
+            name = "Topic VOOZ",
+            back = 2
+            };
+            Console.WriteLine(AnonymoMamaMioPandemio.name, AnonymoMamaMioPandemio.back);
+            var MyDataSupportFarmastor = new
+            {
+                first = "Alexander",
+                second = "Kukhar",
+                state = "Employee of Farmastor",
+                profession = "Technical Support Engineer",
+                workingexperience = "6 months"
+
+            };
+            Console.WriteLine(MyDataSupportFarmastor.first, MyDataSupportFarmastor.workingexperience);
+
+            Action lineactionline = () => Console.WriteLine("Our pandemic carantine ACTION DELEGATE LINE");
+            Action<int, string> lineactionofmylife = (back, description) => { Console.WriteLine($"{back} {description}"); };
+            Predicate<int> NoPositivePredicate = (x) => { return x > 4; };
+            Func<int, int, string> func = delegate (int a, int b) 
+            {
+
+                return (a + b).ToString(); 
+                      
+             };
+
+            lineactionofmylife(4, "predicate tasks");
+            NoPositivePredicate(5);
+            func(1, 2);
+
+
+            string[] fruits = { "peachez", "mangos", "oranges"};
+
+            int minimalLength = fruits.Where(f => f.StartsWith("o"))
+                                .Min(f => f.Length);
+            Console.WriteLine(minimalLength);
+                
+            object[] prepare = { "", 2, '1', 34, 34, 11 };
+
+
+            IEnumerable<object> prepareManyObjectToPeople = from prepa in prepare
+                                                            select prepa;
+            
+            foreach(object quapeople in prepareManyObjectToPeople)
+            {
+                Console.WriteLine(quapeople);
+            }
+            string[] pandemiWorld = { "new", "from", "China", "Europe", "USA", "Ukraine", "Where the end" };
+
+            IEnumerable<string> pandemiQueryLINQ = from pandem in pandemiWorld
+                                                   where pandem == "from"
+                                                   select pandem;
+
+            foreach(string xday in pandemiQueryLINQ)
+            {
+                Console.WriteLine(xday);
+            }
+            Console.ReadKey();
+            //Console.WriteLine("Hello World!");
+
+
+            
+        }
+
+        public int MyDelInMethod(int first, int second, int third)
+        {
+            return first + second + third;
+        }
+    }
+}
